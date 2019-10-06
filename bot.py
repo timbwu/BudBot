f52a90c7c7710f9abf4dfbf60bd71ead02e94702
Tim Wu <timwu767@gmail.com>
2019-10-05 18:54:15 -0700

    minor changes

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # bot.py
 import os
-from discord.ext import commands
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,6 +10,29 @@ token = os.getenv('DISCORD_TOKEN')
 
 # command trigger
 bot = commands.Bot(command_prefix='!')
+# status = cycle(['S', 'SO', 'SOY', 'SOYB', 'SOYBO', 'SOYBOY', '', 'SOYBOY', ''])
+
+
+# bot ready
+@bot.event
+async def on_ready():
+    # change_status.start()
+    print(f'{bot.user.name} has connected to Discord.')
+
+
+# error handling
+# invalid command
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Invalid command.')
+
+
+# background tasks
+# @tasks.loop(seconds=7)
+# async def change_status():
+#     await bot.change_presence(activity=discord.Game(next(status)))
+
 
 # load extension command
 @bot.command(help='Loads an extension.')
@@ -15,11 +40,13 @@ bot = commands.Bot(command_prefix='!')
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
+
 # unload extension command
 @bot.command(help='Unloads an extension.')
 @commands.has_role('Admin')
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
+
 
 # check for cogs on start
 for filename in os.listdir('./cogs'):
